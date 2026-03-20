96f518f1f8cfc39453eec2b90d95c6c3b7924ab7
L1SARD7 <arthemklimchuk7@gmail.com>
2026-03-20 20:52:52 +0200

    chore: setup eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+    "@typescript-eslint/no-explicit-any": 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn'
+    }
+  }
+);
