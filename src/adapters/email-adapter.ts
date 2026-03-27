b75f22b86c243709db435e7d5c903f698d0757b7
L1SARD7 <arthemklimchuk7@gmail.com>
2026-03-27 20:50:39 +0200

    fix: correct email display in profile and add port to email adapter

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -16,7 +16,7 @@ export const emailAdapter = {
             to: email,
             subject: 'Підтвердіть свою електронну адресу.',
             html: `<h1>Підтвердження адреси електронної пошти</h1> 
-        Щоб підтвердити адрес електронної пошти перейдіть за наступним посиланням: <a href="http://localhost:1235/registration/confirmEmail/${confirmationCode}?userId=${userId}">Посилання (Натисніть на цей текст)</a>
+        Щоб підтвердити адрес електронної пошти перейдіть за наступним посиланням: <a href="http://localhost:${config.PORT}/registration/confirmEmail/${confirmationCode}?userId=${userId}">Посилання (Натисніть на цей текст)</a>
         Якщо ви не реєструвались в нашому сервісі, проігноруйте це повідомлення.`,
         });
     },
