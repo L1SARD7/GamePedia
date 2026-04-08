7f47d8e7c5675b03ee705c274e09e8701463ad9e
L1SARD7 <arthemklimchuk7@gmail.com>
2026-04-08 22:06:21 +0300

    fix: update configs for tests

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
-process.env.JWT_SECRET = process.env.JWT_SECRET || 'test-jwt-secret';
-process.env.GOOGLE_APP_USER = process.env.GOOGLE_APP_USER || 'test@example.com';
-process.env.GOOGLE_APP_PASSWORD = process.env.GOOGLE_APP_PASSWORD || 'test-password';
